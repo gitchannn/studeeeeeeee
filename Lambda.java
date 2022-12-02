ad4dc3f8e32d111f1d577e8fe0bf6bb73faf4558
Eunkeee <eunkeeeee@gmail.com>
2022-12-02 15:26:02 +0900

    람다와 스트림

---
+++ Lambda.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class Lambda {
+
+    public static void main(String[] args) {
+        Supplier<Integer> supplier = () -> (int) (Math.random() * 100) + 1;
+        Consumer<Integer> consumer = i -> System.out.print(i + ", ");
+        Predicate<Integer> predicate = i -> i % 2 == 0;
+        Function<Integer, Integer> function = i -> i / 10 * 10;
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(supplier.get());
+        }
+        System.out.println(list);
+
+        for (int i : list) {
+            if (predicate.test(i)) {
+                consumer.accept(i);
+            }
+        }
+        System.out.println();
+
+        List<Integer> newList = new ArrayList<>();
+        for (int i : list) {
+            newList.add(function.apply(i));
+        }
+        System.out.println(newList);
+
+    }
+
+}
---
+++ out/production/woowa-eunkeeee/README.md
@@ -0,0 +1,20 @@
+# woowa-eunkeeee
+
+### 공부해야 할 것 (읽다가 궁금한 것)
+
+- [ ] 싱글톤 패턴
+- [ ] Dto 사용
+- [ ] [정적 팩토리 메서드](https://tecoble.techcourse.co.kr/post/2020-05-26-static-factory-method/)
+
+### 빨리빨리
+
+- [스트림으로 중복값 찾기](https://developer-talk.tistory.com/398)
+- [Char와 Ascii Code](https://blog.naver.com/PostView.nhn?blogId=jysaa5&logNo=221831226674)
+
+### 두고두고 읽어볼 링크
+
+- [Setter를 지양하자?](https://velog.io/@backfox/setter-%EC%93%B0%EC%A7%80-%EB%A7%90%EB%9D%BC%EA%B3%A0%EB%A7%8C-%ED%95%98%EA%B3%A0-%EA%B0%80%EB%B2%84%EB%A6%AC%EB%A9%B4-%EC%96%B4%EB%96%A1%ED%95%B4%EC%9A%94)
+
+### 잊지말자 피드백
+
+- Collections를 `getter` 통해 전달해야 할 때: `Collections.unmodifiableList(computerNumber);`
